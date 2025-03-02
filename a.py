5bef741631d91846e0c8834b4efffbc86706105a
Dilshan Kavinda <dilshankavinda6781@gmail.com>
2025-03-02 20:34:49 +0530

    10 nadun

--- a.py
+++ a.py
@@ -1,7 +1,6 @@
 print("kavinda")
 print("nadun")
 print("nadun")
-<<<<<<< HEAD
 print("nadun")
 print("nadun")
 print("nadun")
@@ -10,5 +9,3 @@ a = [1,2,3]
 
 for i in a:
     print(i)
-=======
->>>>>>> a080752a435e3f43a1e23ed39b6d45e73625f3c2
