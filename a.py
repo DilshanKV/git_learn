985c389119aad43a7c3e9d91d2299337e376b6b6
dilshan-mihcm <dilshan@mihcm.com>
2025-03-02 08:10:33 +0530

    5 nadun

--- a.py
+++ a.py
@@ -7,17 +7,4 @@ print("developer 1")
 
 #im adding new lines.
 
-#i am dilshan.
-#this is my working branch.
-#i am the developer 1
-
-# im nadun.
-# i added this line
-
-print("nadun added new code")
-print("nadun added new code")
-
-
-print("nadun added new code")
-print("nadun added new code")
-
+# i deleted lots of lines
